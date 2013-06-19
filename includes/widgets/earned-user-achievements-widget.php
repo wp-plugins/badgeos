155c0b9c9a5fc62e22c2c6d6a3c9ea7cf28773c9
credly <credly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-19 20:49:30 +0000

    released 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/badgeos/tags/1.1.0@728537 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/widgets/earned-user-achievements-widget.php
@@ -0,0 +1,146 @@
+<?php
+//widget displays achievements earned for the logged in user
+class earned_user_achievements_widget extends WP_Widget {
+
+	//process the new widget
+	function earned_user_achievements_widget() {
+		$widget_ops = array(
+			'classname' => 'earned_user_achievements_class',
+			'description' => __( 'Displays all achievements earned by the logged in user', 'badgeos' )
+		);
+		$this->WP_Widget( 'earned_user_achievements_widget', __( 'BadgeOS Earned User Achievements', 'badgeos' ), $widget_ops );
+	}
+
+	//build the widget settings form
+	function form( $instance ) {
+		$defaults = array( 'title' => 'My Badges', 'number' => '10', 'point_total' => '' );
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = $instance['title'];
+		$number = $instance['number'];
+		$point_total = $instance['point_total'];
+		?>
+            <p><?php _e( 'Title', 'badgeos' ); ?>: <input class="widefat" name="<?php echo $this->get_field_name( 'title' ); ?>"  type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+			<p><?php _e( 'Number to Display (0 = all)', 'badgeos' ); ?>: <input class="widefat" name="<?php echo $this->get_field_name( 'number' ); ?>"  type="text" value="<?php echo absint( $number ); ?>" /></p>
+			<p><label for="<?php echo $this->get_field_name( 'point_total' ); ?>"><input type="checkbox" id="<?php echo $this->get_field_name( 'point_total' ); ?>" name="<?php echo $this->get_field_name( 'point_total' ); ?>" <?php checked( $point_total, 'on' ); ?> /> <?php _e( 'Display user\'s total points', 'badgeos' ); ?></label></p>
+        <?php
+	}
+
+	//save and sanitize the widget settings
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['number'] = absint( $new_instance['number'] );
+		$instance['point_total'] = ( ! empty( $new_instance['point_total'] ) ) ? sanitize_text_field( $new_instance['point_total'] ) : '';
+
+		return $instance;
+	}
+
+	//display the widget
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		echo $before_widget;
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; };
+
+		//user must be logged in to view earned badges and points
+		if ( is_user_logged_in() ) {
+
+			//display user's points if widget option is enabled
+			if ( $instance['point_total'] == 'on' )
+				echo '<p class="badgeos-total-points">' . sprintf( __( 'My Total Points: %s', 'badgeos' ), '<strong>' . number_format( badgeos_get_users_points() ) . '</strong>' ) . '</p>';
+
+			$achievements = badgeos_get_user_achievements();
+
+			if ( is_array( $achievements ) && ! empty( $achievements ) ) {
+
+				$number_to_show = absint( $instance['number'] );
+				$thecount = 0;
+
+				wp_enqueue_script( 'badgeos-achievements' );
+				wp_enqueue_style( 'badgeos-widget' );
+
+				echo '<ul class="widget-achievements-listing">';
+				foreach ( $achievements as $achievement ) {
+
+					//exclude step CPT entries from displaying in the widget
+					if ( get_post_type( $achievement->ID ) != 'step' ) {
+
+						$permalink = get_permalink( $achievement->ID );
+						$title = get_the_title( $achievement->ID );
+
+						$thumb = '';
+						$image_attr = wp_get_attachment_image_src( get_post_thumbnail_id( $achievement->ID ), array( 50, 50 ) );
+
+						if ( $image_attr ) {
+
+							$img = '<img class="wp-post-image" width="'. absint( $image_attr[1] ) .'" height="'. absint( $image_attr[2] ) .'" src="'. esc_url( $image_attr[0] ) .'">';
+							$thumb = '<a style="margin-top: -'. floor( absint( $image_attr[2] ) / 2 ) .'px;" class="badgeos-item-thumb" href="'. esc_url( $permalink ) .'">' . $img .'</a>';
+						}
+
+						// is this achievement Credly giveable?
+						$giveable = credly_is_achievement_giveable( $achievement->ID );
+						$class = 'widget-badgeos-item-title';
+						$item_class = $image_attr ? ' has-thumb' : '';
+						$item_class .= $giveable ? ' share-credly addCredly' : '';
+						$credly_ID = $giveable ? 'data-credlyid="'. absint( $achievement->ID ) .'"' : '';
+
+						echo '<li id="widget-achievements-listing-item-'. absint( $achievement->ID ) .'" '. $credly_ID .' class="widget-achievements-listing-item'. esc_attr( $item_class ) .'">';
+
+						echo $thumb;
+						echo '<a class="widget-badgeos-item-title '. esc_attr( $class ) .'" href="'. esc_url( $permalink ) .'">'. esc_html( $title ) .'</a>';
+						echo '</li>';
+
+						$thecount++;
+
+						if ( $thecount == $number_to_show && $number_to_show != 0 )
+							break;
+
+					}
+				}
+
+				echo '</ul><!-- widget-achievements-listing -->';
+
+			}
+
+		} else {
+
+			//user is not logged in so display a message
+			_e( 'You must be logged in to view earned achievements', 'badgeos' );
+
+		}
+
+		echo $after_widget;
+	}
+
+}
+
+add_action( 'wp_ajax_achievement_send_to_credly', 'badgeos_send_to_credly_handler' );
+add_action( 'wp_ajax_nopriv_achievement_send_to_credly', 'badgeos_send_to_credly_handler' );
+/**
+ * hook in our credly ajax function
+ */
+function badgeos_send_to_credly_handler() {
+
+	if ( ! isset( $_REQUEST['ID'] ) ) {
+		echo json_encode( sprintf( '<strong class="error">%s</strong>', __( 'Error: Sorry, nothing found.', 'badgeos' ) ) );
+		die();
+	}
+
+	$send_to_credly = $GLOBALS['badgeos_credly']->post_credly_user_badge( $_REQUEST['ID'] );
+
+	if ( $send_to_credly ) {
+
+		echo json_encode( sprintf( '<strong class="success">%s</strong>', __( 'Success: Sent to Credly!', 'badgeos' ) ) );
+		die();
+
+	} else {
+
+		echo json_encode( sprintf( '<strong class="error">%s</strong>', __( 'Error: Sorry, Send to Credly Failed.', 'badgeos' ) ) );
+		die();
+
+	}
+}
